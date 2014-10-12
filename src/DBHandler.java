8b4b50904d6fd2b7d177f5f7912b4ecf336168e9
ladyalpineblue <katrineorlova@gmail.com>
2014-10-12 17:30:31 +0200

    test2

--- src/DBHandler.java
+++ src/DBHandler.java
@@ -1,7 +1,6 @@
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DBHandler {
 
@@ -13,32 +12,40 @@ public class DBHandler {
         db = new ConnectToDB("localhost", "world", user, password);
     }
 
-
-
     public void showTable(String tableName) throws SQLException {
 
-        String initialQuery = "show columns from " + tableName;
-        String query = "SELECT ";
-        List<String> columns = new ArrayList<>();
+        String query = "SELECT * FROM " + tableName;
         try (Connection connection = db.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(initialQuery)) {
+             ResultSet resultSet = statement.executeQuery(query);) {
+            //ResultSetMetaData rsmd = resultSet.getMetaData();
+            TableReader reader = new TableReader(resultSet.getMetaData());
             while (resultSet.next()) {
-               columns.add(resultSet.getString(1));
+                reader.showLine(resultSet);
             }
-
-            for(String column: columns){
-                query += column + ", ";
-            }
-
-            query = query.substring(0, query.length()-2) + " from " + tableName;
-            System.out.println(query);
-            Statement statement2 = connection.createStatement();
-            ResultSet resultSet2 = statement.executeQuery(query);
-                while (resultSet2.next()) {
-                    System.out.println(resultSet2.getString(1));
-                }
         }
     }
 
+    public void copyFile(String fileName, String tableName) throws SQLException {
+        FileParser fileToCopy = new FileParser(fileName);
+        //String sql = "CREATE TABLE " + tableName + " IF NOT EXISTS;";
+
+        PreparedStatement preparedStatement = null;
+        try (Connection connection = db.getConnection()) {
+            preparedStatement = connection
+                    .prepareStatement("CREATE TABLE IF NOT EXISTS" + tableName + ";
+            preparedStatement = connection
+                    .prepareStatement("insert into " + tableName + " values (default, ?, ?, ?, ? , ?, ?)");
+            // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+            // parameters start with 1
+            preparedStatement.setString(1, "Test");
+            preparedStatement.setString(2, "TestEmail");
+            preparedStatement.setString(3, "TestWebpage");
+            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
+            preparedStatement.setString(5, "TestSummary");
+            preparedStatement.setString(6, "TestComment");
+            preparedStatement.executeUpdate();
+
+        }}
+*/
 }
